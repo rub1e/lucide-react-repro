877946798803cd75e402ad25ff5a59020cb3057d
rub1e <alex.rubner@gmail.com>
2025-01-17 13:14:54 +0000

    add lucide

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,6 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import { StarIcon } from "lucide-react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +10,9 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div>
+      <StarIcon />
+    </div>
+  );
 }
